7ef443b11f5dc73e6cc3d42a4146614a61d0630f
matthew-laine <matthew.i.laine@gmail.com>
2019-05-29 16:19:49 -0400

    can add employees to hospital and refer to hashmap to get employee name

---
+++ src/main/java/Hospital.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+public class Hospital {
+
+	private HashMap<Integer, Employee> employees = new HashMap();
+
+	public int getNumberOfEmployees() {
+		return 0;
+	}
+	public String getEmployeeName(int employeeNumber) {
+		return employees.get(employeeNumber).getName();
+	}
+
+	public void addJanitor(String name, int employeeNumber) {
+		employees.put(employeeNumber, new Janitor(name, employeeNumber));
+	}
+	public void addNurse(String name, int employeeNumber) {
+		employees.put(employeeNumber, new Nurse(name, employeeNumber));
+	}
+	public void addReceptionist(String name, int employeeNumber) {
+		employees.put(employeeNumber, new Receptionist(name, employeeNumber));	
+	}
+	public void addDoctor(String name, int employeeNumber, String specialty) {
+		employees.put(employeeNumber, new Doctor(name, employeeNumber, specialty));
+	}
+}
